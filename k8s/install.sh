b496536314d89352826f0b0d34d0fce6ca417f5e
Chris Kinsman <chris@kinsman.net>
2021-03-26 14:43:03 -0700

    Fix mongodb chart

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 kubectl apply -f namespace.yaml
-helm install --namespace tilloo-services stable/mongodb --name mongodb --set mongodbRootPassword=kFGJgJEdrE
+helm install mongodb --set mongodbRootPassword=kFGJgJEdrE --namespace tilloo-services stable/mongodb
 sleep 120
 kubectl apply -f app.yaml
