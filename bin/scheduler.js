1fc4c52e3c7b9febcea7272c3888ed3f01910c5f
Chris Kinsman <chris@kinsman.net>
2021-03-30 12:11:04 -0700

    Snapshot in code

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -112,3 +112,15 @@ const _loadedJobs = {};
         process.exit(1);
     }
 })();
+
+const { writeHeapSnapshot } = require('v8');
+function dump() {
+    const fileName = '/var/log/' + Date.now() + '.heapsnapshot';
+    console.info('Writing heapsnapshot to: ' + fileName);
+    writeHeapSnapshot(fileName);
+}
+dump();
+
+setInterval(function () {
+    dump();
+}, 1000 * 60 * 60);
